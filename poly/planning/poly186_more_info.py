1e71f4506e780a41a1e3e58e79b930b5c4bf12e1
Prinecps Polaris <polycapn@gmail.com>
2023-10-01 07:26:11 -0700

    Untracked specified files and updated .gitignore

--- poly/planning/poly186_more_info.py
+++
@@ -1,9 +0,0 @@
-# filename: poly186_more_info.py
-
-from googlesearch import search
-
-def get_urls(query, num_results):
-    urls = [url for url in search(query, num_results=num_results)]
-    return urls
-
-print("\n".join(get_urls("Poly186", 10)))
